9be1156ea74a76b0bd7a8bccaef129e8c96b7791
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-01 22:12:07 +0300

    feat: add swagger

--- passenger-service/src/main/java/com/software/modsen/passengerservice/controller/PassengerController.java
+++ passenger-service/src/main/java/com/software/modsen/passengerservice/controller/PassengerController.java
@@ -6,6 +6,12 @@ import com.software.modsen.passengerservice.dto.response.PassengerResponse;
 import com.software.modsen.passengerservice.mapper.PassengerMapper;
 import com.software.modsen.passengerservice.model.Passenger;
 import com.software.modsen.passengerservice.service.PassengerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +26,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +35,15 @@ public class PassengerController {
     private final PassengerMapper passengerMapper;
 
     @GetMapping("/{id}")
+    @Operation(description = "Get Passenger by ID ",
+            parameters = {@Parameter(name = "id", description = "This is the Passenger ID that will be searched for")})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found Passenger by ID",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PassengerResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Passenger not found",
+                    content = @Content(schema = @Schema(hidden = true)))}
+    )
     public ResponseEntity<PassengerResponse> getPassengerById(@PathVariable Long id) {
         Passenger passenger = passengerService.getPassengerById(id);
         PassengerResponse passengerResponse = passengerMapper.toPassengerResponse(passenger);
@@ -37,13 +51,28 @@ public class PassengerController {
     }
 
     @GetMapping
+    @Operation(description = "Get list of all Passengers ")
+    @ApiResponse(responseCode = "200", description = "List of all Passengers",
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = PassengerListResponse.class))})
     public ResponseEntity<PassengerListResponse> getAllPassengers() {
-        List<Passenger> passengerList =  passengerService.getAllPassengers();
+        List<Passenger> passengerList = passengerService.getAllPassengers();
         List<PassengerResponse> passengerResponseList = passengerMapper.toPassengerResponseList(passengerList);
         return ResponseEntity.ok(new PassengerListResponse(passengerResponseList));
     }
 
     @PostMapping
+    @Operation(description = "Create new Passenger ",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PassengerRequest.class))))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Create Passenger",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PassengerResponse.class))}),
+            @ApiResponse(responseCode = "409", description = "Passenger with email/phone already exist",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     public ResponseEntity<PassengerResponse> createPassenger(@RequestBody PassengerRequest passengerRequest) {
         Passenger newPassenger = passengerMapper.toPassengerEntity(passengerRequest);
         Passenger passenger = passengerService.createPassenger(newPassenger);
@@ -55,11 +84,31 @@ public class PassengerController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(description = "Delete(soft) Passenger bu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Delete Passenger"),
+            @ApiResponse(responseCode = "404", description = "Passenger not found",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     public void deletePassenger(@PathVariable Long id) {
         passengerService.deletePassenger(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(description = "Update Passenger ",
+            parameters = {@Parameter(name = "id", description = "This is the Passenger ID that will be updated")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PassengerRequest.class))))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update Passenger",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PassengerResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Passenger not found",
+                    content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "409", description = "Passenger with email/phone already exist",
+                    content = @Content(schema = @Schema(hidden = true)))
+    })
     public ResponseEntity<PassengerResponse> updatePassenger(
             @PathVariable Long id,
             @RequestBody PassengerRequest passengerRequest
