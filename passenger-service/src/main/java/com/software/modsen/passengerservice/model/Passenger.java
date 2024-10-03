3c00732407bbf1d5249e31cee8318ce7a25bd51f
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-03 10:44:36 +0300

    test: add unit tests

--- passenger-service/src/main/java/com/software/modsen/passengerservice/model/Passenger.java
+++ passenger-service/src/main/java/com/software/modsen/passengerservice/model/Passenger.java
@@ -6,10 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 
 @Entity
@@ -19,6 +16,7 @@ import org.hibernate.annotations.SQLDelete;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
