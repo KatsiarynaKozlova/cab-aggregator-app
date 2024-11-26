e9582fa89124af601cda74fc49550f5e82cf0e6a
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-11-26 12:12:45 +0300

    refactore

--- auth-service/src/main/java/com/software/modsen/authservice/util/KeycloakConstants.java
+++ auth-service/src/main/java/com/software/modsen/authservice/util/KeycloakConstants.java
@@ -10,7 +10,7 @@ public final class KeycloakConstants {
     public static final String REALM_ACCESS = "realm_access";
     public static final String ROLES = "roles";
     public static final String PREFIX_ROLE = "ROLE_";
-    public static final String SERVER_URL = "http://keycloak:8100";
+    public static final String SERVER_URL = "http://localhost:8100";
     public static final String REALM = "cab-aggregator-realm";
     public static final String KEYCLOAK_CLIENT_ID = "spring-gateway-client";
     public static final String KEYCLOAK_CLIENT_SECRET = "R8zZaZmV1sm3vlTGa1Woq7uUbt6LbaAd";
